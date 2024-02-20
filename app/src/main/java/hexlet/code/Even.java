06e9cf2b06382c17e99e9262be6c5f634443c926
fireyork <andrre2001@gmail.com>
2024-02-20 18:38:08 +0300

    add fix game Even

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -4,20 +4,27 @@ import java.util.Random;
 
 public class Even {
     public static void getAnswer() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-        int newValue = generateNumber();
-        System.out.println("Question: " + newValue);
-        String userChoice = scanner.next();
-        System.out.println("Your answer: " + userChoice);
-        if (isEven(newValue) && userChoice.equals("yes")) {
-            System.out.println("Correct!");
+        int rightAns = 0;
+        while (rightAns < 3) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+            int newValue = generateNumber();
+            System.out.println("Question: " + newValue);
+            String userChoice = scanner.next();
+            System.out.println("Your answer: " + userChoice);
+            if (isEven(newValue) && userChoice.equals("yes")) {
+                System.out.println("Correct!");
+                rightAns++;
+            } else if (!isEven(newValue) && userChoice.equals("no")) {
+                System.out.println("Correct!");
+                rightAns++;
+            } else {
+                System.out.println("Let's try again, " + Cli.getName());
+                break;
+            }
         }
-        else if (!isEven(newValue) && userChoice.equals("no")) {
-            System.out.println("Correct!");
-        }
-        else {
-            System.out.println("Let's try again, " + Cli.getName());
+        if (rightAns == 3) {
+            System.out.println("Congratulations, " + Cli.getName());
         }
     }
 
