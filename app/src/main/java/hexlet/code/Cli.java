3395babd936062e4b7dd80ac354ef1535882211e
Andrey Piskarev <andrre2001@gmail.com>
2024-03-13 05:32:15 +0300

    delete getter

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,15 +1,11 @@
 package hexlet.code;
 import java.util.Scanner;
 public class Cli {
-    private static String userName;
     public static void greeting() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
-        userName = scanner.next();
+        String userName = scanner.next();
         System.out.println("Hello, " + userName +  "!");
-    }
-    public static String getName() {
-        return userName;
     }
 }
