69a0ac56b15bc196da4da7354830dd3792eaead1
Andrey Piskarev <andrre2001@gmail.com>
2024-03-19 17:24:19 +0300

    fix final int

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -5,16 +5,16 @@ import java.util.Random;
 
 public class Calc {
 
-    private static final int minNum = 1;
-    private static final int maxNum = 100;
-    private static final int variants = 3;
+    private static final int MIN_NUM = 1;
+    private static final int MAX_NUM = 100;
+    private static final int VARIANTS = 3;
     private static String question;
     public static void gameCalc() {
         String[][] gameData = new String[Engine.getMaxCount()][2];
         String gameDescription = "What is the result of the expression?";
         for (int i = 0; i < Engine.getMaxCount(); i++) {
-            int num1 = Utils.generateNum(minNum, maxNum);
-            int num2 = Utils.generateNum(minNum, maxNum);
+            int num1 = Utils.generateNum(MIN_NUM, MAX_NUM);
+            int num2 = Utils.generateNum(MIN_NUM, MAX_NUM);
             char sing = generateSing();
             gameData[i][1] = Integer.toString(getTask(num1, num2, sing));
             gameData[i][0] = question;
@@ -46,6 +46,6 @@ public class Calc {
     private static char generateSing() {
         Random rand = new Random();
         char[] operators = {'+', '-', '*'};
-        return operators[rand.nextInt(variants)];
+        return operators[rand.nextInt(VARIANTS)];
     }
 }
