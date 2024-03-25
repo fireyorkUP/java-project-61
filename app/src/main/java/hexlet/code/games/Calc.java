ada65e784f8558cb61692a7f7869db0e0cf97fda
Andrey Piskarev <andrre2001@gmail.com>
2024-03-26 02:30:49 +0300

    fix names of methods

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,14 +1,13 @@
 package hexlet.code.games;
 import hexlet.code.Engine;
 import hexlet.code.Utils;
-import java.util.Random;
 
 public class Calc {
 
     private static final int MIN_NUM = 1;
     private static final int MAX_NUM = 100;
     private static final int VARIANTS = 3;
-    private static String question;
+    private static char[] operators = {'+', '-', '*'};
     public static void gameCalc() {
         String[][] gameData = new String[Engine.getMaxCount()][2];
         String gameDescription = "What is the result of the expression?";
@@ -16,36 +15,23 @@ public class Calc {
             int num1 = Utils.generateNum(MIN_NUM, MAX_NUM);
             int num2 = Utils.generateNum(MIN_NUM, MAX_NUM);
             char sing = generateSing();
-            gameData[i][1] = Integer.toString(getTask(num1, num2, sing));
-            gameData[i][0] = question;
+            gameData[i][1] = Integer.toString(calculate(num1, num2, sing));
+            gameData[i][0] = num1 + " " + sing + " " + num2;
         }
         Engine.newGame(gameDescription, gameData);
     }
 
-    private static int getTask(int num1, int num2, char sing) {
-        int resultNum = 0;
-        switch (sing) {
-            case ('-'):
-                question = num1 + " - " + num2;
-                resultNum = num1 - num2;
-                break;
-            case ('+'):
-                question = num1 + " + " + num2;
-                resultNum = num1 + num2;
-                break;
-            case ('*'):
-                question = num1 + " * " + num2;
-                resultNum = num1 * num2;
-                break;
-            default:
-                break;
-        }
-        return resultNum;
+    private static int calculate(int num1, int num2, char sing) {
+        return switch (sing) {
+            case ('-') -> num1 - num2;
+            case ('+') -> num1 + num2;
+            case ('*') -> num1 * num2;
+            default -> throw new Error("Unknown order state: " + sing + " !");
+        };
     }
 
     private static char generateSing() {
-        Random rand = new Random();
-        char[] operators = {'+', '-', '*'};
-        return operators[rand.nextInt(VARIANTS)];
+        return operators[Utils.generateNum(MIN_NUM, VARIANTS) - 1];
     }
 }
+
--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,6 +1,7 @@
 package hexlet.code.games;
 import hexlet.code.Engine;
 import hexlet.code.Utils;
+
 import java.util.Random;
 
 public class Progression {
